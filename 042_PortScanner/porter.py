254f968e9e98eb0c2519a312f121edcd0d77672d
sararin <sararin@cock.li>
2018-09-09 19:35:54 +0200

    basic range added

--- 042_PortScanner/porter.py
+++ 042_PortScanner/porter.py
@@ -6,12 +6,13 @@ class Porter:
   def __init__(self, addressPorts):
     self.address = addressPorts[0]
     self.ports = addressPorts[1]
-    print(self.ports)
 
   def runScan(self):
     for addr in self.address:
       for prt in self.ports:
+        print(addr, end = ': ')
         self._checkPort(addr, prt)
+        print()
 
   def _checkPort(self, addr, prt):
     try:
@@ -19,9 +20,9 @@ class Porter:
         sock.settimeout(3)
         result = sock.connect_ex((addr, prt))
         if result == 0:
-          print(prt, "open")
+          print(prt, "open", end = '')
         else:
-          print(prt, "closed")
+          print(prt, "closed", end = '')
     except socket.gaierror:
       print("Hostname couldn't be resolved")
       sys.exit()
@@ -34,19 +35,24 @@ class Expander:
     self.address = address
     self.ports = ports
 
-  def _expand(self, thingToExpand):
-    if "-" in thingToExpand:
-      temp = tuple(thingToExpand.split('-'))
+  def _expandPort(self, rangePorts):
+    if "-" in rangePorts:
+      temp = tuple(rangePorts.split('-'))
       return list(range(int(temp[0]), int(temp[1])))
-    return (int(thingToExpand), int(thingToExpand)+1)
+    return [int(rangePorts)]
+
+  def _expandAddr(self, rangeAddr):
+    if '-' in rangeAddr:
+      temp = [x.split('-') if "-" in x else x for x in self.address.split('.')]
+      first = [ x[0] if isinstance(x, list) else x for x in temp]
+      last = [ x[1] if isinstance(x, list) else x for x in temp]
+      return [ str(ipaddress.IPv4Address(ipadd)) for ipadd in range(int(ipaddress.IPv4Address('.'.join(first))), int(ipaddress.IPv4Address('.'.join(last))))]
+    return [rangeAddr]
+
 
   def _prepareOutput(self):
-    self.ports = self._expand(self.ports)
-    self.address = [self.address]
-    #temp = [x.split('-') if "-" in x else x for x in self.address.split('.')]
-    #first = [ x[0] if isinstance(x, list) else x for x in temp]
-    #last = [ x[1] if isinstance(x, list) else x for x in temp]
-    #self.address = (ipaddress.IPv4Address('.'.join(first)), ipaddress.IPv4Address('.'.join(last)))
+    self.ports = self._expandPort(self.ports)
+    self.address = self._expandAddr(self.address)
 
 
   def giveBack(self):
