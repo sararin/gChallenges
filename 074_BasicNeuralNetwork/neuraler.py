1db99386cb1aa6272cdba2d3fc5ba45628f5fe1b
sararin <sararin@cock.li>
2018-09-13 20:10:19 +0200

    before forward backpropagation

--- 074_BasicNeuralNetwork/neuraler.py
+++ 074_BasicNeuralNetwork/neuraler.py
@@ -1,9 +1,30 @@
+import random 
+from functools import reduce
+
 class Neuron:
-  def __init__(self, value, connTo, weight):
+  def __init__(self, value, weight, connTo=None):
     self.value = value
     self.connTo = connTo
     self.weight = weight
 
+  def giveValue(self):
+    return self.value * self.weight
+
+def addAll(argv):
+  temp = []
+  for x in argv:
+    temp.append(x.giveValue())
+  return reduce(lambda x,y: x+y, temp)
+  
+def uniform(x):
+  return x
 
 if __name__ == "__main__":
-  pass
+  inputs = [ Neuron(1, random.uniform(0, 1)), Neuron(1, random.uniform(0, 1)) ]
+  hidden = [ Neuron(1, random.uniform(0, 1)), Neuron(1, random.uniform(0, 1)), Neuron(1, random.uniform(0, 1)) ]
+  output = [ Neuron(1, 1) ]
+  for x in hidden:
+    x.value = uniform(addAll(inputs))
+  for x in output:
+    x.value = uniform(addAll(hidden))
+  print(output[0].value)
